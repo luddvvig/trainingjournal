a70d2127b6b1a82bd023888589098a5cd3aa2abc
Ludwig <luddwig97@gmail.com>
2025-02-12 23:46:33 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#TrainingJournal
+import datetime
+import sys
+
+#For kicking out intruders!
+def exit_program():
+        print("Program is being shut down")
+        sys.exit()
+
+#I need to welcome the user(me)
+def welcome():
+    time = datetime.date.today()
+    formated_date = time.strftime("%m/%d/%y")
+    name = input("Enter your name: ")
+
+    if name.lower() == "ludwig":
+        print(f"Welcome back {name}!")
+        print(f"Today is {formated_date}")
+        print("What do you want to do today?")
+        choice = input("Write a new log? (W)\nLook at an old log? (NL)\nUpdate status? (U)")
+    else:
+        print(f"WARNING!! {name}, you are not authorized!")
+        exit_program()
+    
+welcome()
