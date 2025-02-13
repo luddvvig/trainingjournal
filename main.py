4a246dc5d1f9cb7b8e1875804f8e5674bdb339d1
Ludwig <luddwig97@gmail.com>
2025-02-13 20:41:12 +0100

    Added a function to write daily logs, and a ignorefil to stop tracking my traininglogs.

--- main.py
+++ main.py
@@ -17,15 +17,29 @@ def welcome():
         print(f"Welcome back {name}!")
         print(f"Today is {formated_date}")
         print("What do you want to do today?")
-        choice = input("Write a new log? (W)\nLook at an old log? (NL)\nUpdate status? (U)")
+        choice = input("Write a new log? (W)\nLook at an old log? (R)\nUpdate status? (U)")
+        if choice.lower() == "w":
+             write_log(name)
+        elif choice.lower() == "r":
+             "read_log()"
+        else:
+             print("Use\nWrite a new log? (W)\nLook at an old log? (R)\nUpdate status? (U)")
     else:
         print(f"WARNING!! {name}, you are not authorized!")
         exit_program()
 
-def write_log():
+def write_log(name):
+     if name.lower() == "ludwig":
+          with open("status.txt", "a") as status_file:
+               status_file.write(input("Write a short log about todays training here: "))
+               status_file.write("\n")
+               status_file.write((input("\nHow long was the training? In H here: ")))
+               status_file.write("\n")
+               status_file.write(input("What was the date of the session? dd/mm/yy"))
+               status_file.write("\n")
 
-def read_log():
+'''def read_log():
 
-def status():
+def status():'''
 
 welcome()
