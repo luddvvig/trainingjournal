6bc9d49c07192a0a72da29fbab3832be724741d6
Ludwig <luddwig97@gmail.com>
2025-02-16 21:12:36 +0100

    Addes read log function

--- main.py
+++ main.py
@@ -4,42 +4,72 @@ import sys
 
 #For kicking out intruders!
 def exit_program():
-        print("Program is being shut down")
-        sys.exit()
+     print("Program is being shut down")
+     sys.exit()
 
 #I need to welcome the user(me)
 def welcome():
-    time = datetime.date.today()
-    formated_date = time.strftime("%m/%d/%y")
-    name = input("Enter your name: ")
+     time = datetime.date.today()
+     formated_date = time.strftime("%m/%d/%y")
+     name = input("Enter your name: ")
+     return name, formated_date
 
-    if name.lower() == "ludwig":
-        print(f"Welcome back {name}!")
-        print(f"Today is {formated_date}")
-        print("What do you want to do today?")
-        choice = input("Write a new log? (W)\nLook at an old log? (R)\nUpdate status? (U)")
-        if choice.lower() == "w":
-             write_log(name)
-        elif choice.lower() == "r":
-             "read_log()"
-        else:
-             print("Use\nWrite a new log? (W)\nLook at an old log? (R)\nUpdate status? (U)")
-    else:
-        print(f"WARNING!! {name}, you are not authorized!")
-        exit_program()
+def main_menu(name, formated_date):
+     while True:
+          if name.lower() == "ludwig":
+               print(f"Welcome back {name}!")
+               print(f"Today is {formated_date}")
+               print("What do you want to do today?")
+               choice = input("Write a new log? (W)\nLook at an old log? (R)\nUpdate status? (U)\nQuit? (Q) ")
+               if choice.lower() == "w":
+                    write_log(name)
+                    continue
+               elif choice.lower() == "r":
+                    read_log()
+               elif choice.lower() == "q":
+                    exit_program()
+               else:
+                    print("Invald input, try again!")
+                    print("Use\nWrite a new log? (W)\nLook at an old log? (R)\nUpdate status? (U)")
+          else:
+               print(f"WARNING!! {name}, you are not authorized!")
+               exit_program()
 
 def write_log(name):
-     if name.lower() == "ludwig":
-          with open("status.txt", "a") as status_file:
-               status_file.write(input("Write a short log about todays training here: "))
-               status_file.write("\n")
-               status_file.write((input("\nHow long was the training? In H here: ")))
-               status_file.write("\n")
-               status_file.write(input("What was the date of the session? dd/mm/yy"))
-               status_file.write("\n")
+     with open("status.txt", "a") as log_file:
+          log_file.write("\n")
+          log_file.write(input("What was the date of the session? dd/mm/yy: "))
+          log_file.write("\n")
+          log_file.write(input("Write a short log about todays training here: "))
+          log_file.write("\n")
+          log_file.write((input("\nHow long was the training? In H here: ")))
+          log_file.write("\n")
+          log_file.write(f"{name}")
+          log_file.write("\n")
+          
+          
+def read_log():
+     date = input("What date do you want to check out? (dd/mm/yy): ")
+     with open("status.txt", "r") as file:
+          lines = file.readlines()
+          nmr_line = -1  
 
-'''def read_log():
+          for i, row in enumerate(lines):  
+               if date in row: 
+                    nmr_line = i
+                    break 
 
-def status():'''
+          if nmr_line != -1:
+               
+               for i in range(nmr_line, min(nmr_line + 3, len(lines))):
+                    print(lines[i].strip())
+          else:
+               print(f"No log found for date: {date}")
 
-welcome()
+#def status():
+def main():
+     name, formated_date = welcome()
+     main_menu(name, formated_date)
+
+if __name__ == "__main__":
+     main()
